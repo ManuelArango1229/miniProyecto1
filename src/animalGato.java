aba4e150a85ddbb19bb55fe769cdce1c4711b31f
Juan Manuel Arango <juan12292004@outlook.com>
2023-04-23 10:24:43 -0500

    se crea e implementa interfaz para las vacunas con un arraylist, se le añanade el atributo nombre a animalVeterinaria

--- src/animalGato.java
+++ src/animalGato.java
@@ -1,8 +1,8 @@
 /////clase para crear objetos de tipo gato 
 
 public class animalGato extends animalVeterinaria{
-    public animalGato(Double precio, String origen,String raza,short cantidadDientes){
-        super(precio,origen);
+    public animalGato(Double precio, String origen,String nombre,String raza,short cantidadDientes){
+        super(precio,origen,nombre);
         this.cantidadDientes=cantidadDientes;
         this.raza=raza;
     }
