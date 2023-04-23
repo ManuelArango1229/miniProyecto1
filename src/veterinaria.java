b6b5c7587168838ec81114052b49895575c9422f
Nicolle1101 <nicolle.lopez@correounivalle.edu.co>
2023-04-23 09:45:56 -0500

    Primera implementación interfaz

--- src/veterinaria.java
+++ src/veterinaria.java
@@ -5,6 +5,14 @@ public class veterinaria {
         
     }
     public static void consola() {
-        
+        System.out.println("\t\t _____________________________");
+        System.out.println("\t\t|    V E T E R I N A R I A    | ");
+        System.out.println("\t\t| 1.Insertar mascota          |");
+        System.out.println("\t\t| 2.Actualizar mascota        |");
+        System.out.println("\t\t| 3.Eliminar mascota          |");
+        System.out.println("\t\t| 4.Buscar mascota por nombre |");
+        System.out.println("\t\t| 5.Listar todas las mascotas |");
+        System.out.println("\t\t| 6.Salir                     |");
+        System.out.println("\t\t|_____________________________|");
     }
 }
